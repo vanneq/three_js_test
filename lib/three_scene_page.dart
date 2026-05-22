6951dea1eaba7d7229acf6efe5a222ec4eec338a
polin <polinasemushevaa@gmail.com>
2026-05-22 16:07:19 +0500

    feat: setup basic 3D scene with animated cube

--- lib/three_scene_page.dart
+++ lib/three_scene_page.dart
@@ -12,19 +12,29 @@ class ThreeScenePage extends StatefulWidget {
 class _ThreeScenePageState extends State<ThreeScenePage> {
   late three.ThreeJS threeJS;
   late three.Mesh cube;
+  bool _isReady = false;
+
   @override
   void initState() {
     super.initState();
     threeJS = three.ThreeJS(
       onSetupComplete: () {
-        setState(() {});
+        setState(() {
+          _isReady = true;
+        });
       },
 
       setup: setup,
+      settings: three.Settings(
+
+      )
     );
   }
 
+
   Future<void> setup() async {
+    threeJS.scene = three.Scene();
+
     threeJS.camera = three.PerspectiveCamera(
       75,
       threeJS.width / threeJS.height,
@@ -35,13 +45,22 @@ class _ThreeScenePageState extends State<ThreeScenePage> {
     final geometry = three.BoxGeometry(1, 1, 1);
     final material = three.MeshBasicMaterial.fromMap({
       "color": three.Color(0, 1, 0),
+
     });
+    final edges = three.EdgesGeometry(geometry, 15);
+    final lineMaterial = three.LineBasicMaterial.fromMap({
+      "color": three.Color(1 ,1,1)
+    });
+    final wireframe = three.LineSegments(edges, lineMaterial);
+
     cube = three.Mesh(geometry, material);
     threeJS.scene.add(cube);
+    cube.add(wireframe);
 
     threeJS.addAnimationEvent((dt) {
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
+
     });
   }
 
@@ -53,6 +72,22 @@ class _ThreeScenePageState extends State<ThreeScenePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: threeJS.build());
+    return Scaffold(
+      body: Stack(
+        children: [
+          threeJS.build(),
+          if (!_isReady)
+            const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("loading"),
+                  CircularProgressIndicator(),
+                ],
+              ),
+            ),
+        ],
+      ),
+    );
   }
 }
